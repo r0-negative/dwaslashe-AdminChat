f3784964ae9a0845d4c171b768473989b4328734
dwa slashe <63373312+Gacus222@users.noreply.github.com>
2021-05-18 15:21:40 +0200

    dwaslashe-AdminChatSpigot

--- src/xyz/dwaslashe/adminchat/Main.java
+++ src/xyz/dwaslashe/adminchat/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.dwaslashe.adminchat.utils.Api;
@@ -57,5 +58,11 @@ public class Main extends JavaPlugin implements Listener {
                 }
             }
         }
+    }
+    @EventHandler
+    public void TestGithuba(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+        event.setJoinMessage(null);
+        p.sendMessage("witoj testowa");
     }
 }
